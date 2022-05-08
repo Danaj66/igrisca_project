fe28844935ba71abf1cbeaefd0fb1f4731ccccf2
Danaj66 <64786952+Danaj66@users.noreply.github.com>
2022-05-08 17:33:40 +0200

    Login
    ustvaril projekt v IntellIj in didal loginFrame

---
+++ aplikacija/src/loginFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class loginFrame {
+    private JPanel loginPanel;
+    private JTextField tfMail;
+    private JPasswordField tfPass;
+    private JButton btnLogin;
+}
